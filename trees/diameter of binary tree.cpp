b0cc9661e3d1413fdf2b94b908fe70be1620d881
Lovely Mangal <76448141+lovely57@users.noreply.github.com>
2022-05-08 14:53:54 +0530

    Update diameter of binary tree.cpp

--- trees/diameter of binary tree.cpp
+++ trees/diameter of binary tree.cpp
@@ -1,3 +1,30 @@
+//brute force
+//tc- o(n2)
+class Solution {
+public:
+    int height(TreeNode*root){
+        if(!root) return 0;
+        return 1+max(height(root->left),height(root->right));   
+    }
+//     preorder traversal
+    void solve(TreeNode*root,int &ans){
+          if(!root) return;
+        int left = height(root->left);
+        int right = height(root->right);
+       ans = max(ans,(left+right));  //as question mention about edges
+        solve(root->left,ans);
+        solve(root->right,ans);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+      int ans = 0;
+        solve(root,ans);
+        return ans;
+    }
+};
+
+//optimised solution 
+//tc- o(n)
+//sc-o(n)
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
@@ -21,3 +48,6 @@ private:
          
      }
 };
+
+
+
