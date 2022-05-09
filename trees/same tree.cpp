bd4b9679c124125692d5cbcaf4692a594accab40
Lovely Mangal <76448141+lovely57@users.noreply.github.com>
2022-05-10 02:42:23 +0530

    Add files via upload

---
+++ trees/same tree.cpp
@@ -0,0 +1,36 @@
+//recursive 
+//tc-o(2*n)=o(n) sc-o(1)
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL ) return true;
+        if(p==NULL || q==NULL )  return false;
+        if(p->val !=q->val)       return false;
+        return isSameTree(p->right,q->right) && isSameTree(p->left,q->left);
+    }
+};
+
+//iterative 
+//tc,sc-o(n)
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+           queue<TreeNode *> queue;
+          queue.push(p);
+          queue.push(q);
+          while (queue.size()!=0){
+              TreeNode * q2=queue.front();
+              queue.pop();
+              TreeNode * q1=queue.front();
+              queue.pop();
+              if (q1==nullptr && q2==nullptr) continue;
+              if (q1==nullptr || q2==nullptr) return false;
+              if (q1->val!=q2->val) return false;
+              queue.push(q1->left);
+              queue.push(q2->left);
+              queue.push(q1->right);
+              queue.push(q2->right);
+          }
+          return true;
+    }
+};
