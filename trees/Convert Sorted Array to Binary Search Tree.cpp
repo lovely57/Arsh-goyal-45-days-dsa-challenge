b60bb249ccd17164206f3f4cf24ef580a332ced6
Lovely Mangal <76448141+lovely57@users.noreply.github.com>
2022-05-09 23:53:50 +0530

    Add files via upload

---
+++ trees/Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,19 @@
+//recursive using binary search
+//tc- o(n)
+//sc- o(1)
+class Solution {
+    public:
+    TreeNode*rec(vector<int>&nums,int start,int end)
+    {
+        if(start>=end) return NULL;
+        int mid=(start+end)/2;
+        TreeNode* node=new TreeNode(nums[mid]);
+        node->left=rec(nums,start,mid);
+        node->right=rec(nums,mid+1,end);
+        return node;
+        
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return rec(nums,0,nums.size());
+    }
+};
