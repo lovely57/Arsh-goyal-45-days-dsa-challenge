c9737f379ad6009b59b42aada5b8d6ebc23b3b78
Lovely Mangal <76448141+lovely57@users.noreply.github.com>
2022-05-10 23:57:26 +0530

    Add files via upload

---
+++ trees/Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -0,0 +1,34 @@
+//recursive
+//tc-o(h) as we r traversing in bst and not all nodes
+//sc- o(1)
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p->val <root->val && q->val<root->val)
+            return lowestCommonAncestor(root->left,p,q);
+        else if(p->val >root->val && q->val>root->val)
+             return lowestCommonAncestor(root->right,p,q);
+        else
+            return root;
+    }
+};
+
+//iterative
+//tc- o(h)
+//sc-o(1)
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root != NULL){
+            if(root->val > p->val and root->val > q->val){
+                root = root->left;
+            }    
+            else if(root->val < p->val and root->val < q->val){
+                root = root->right;
+            }
+            else
+                break;
+        }
+        return root;
+    }
+};
